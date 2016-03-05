e9d5e805e328f0b63aae61ff84b3cdcfd5e87851
Carlos G. Rodriguez <carlosro.ec@gmail.com>
2016-03-04 19:38:36 -0500

    Add forms progress

--- app/Http/Controllers/CampainController.php
+++ app/Http/Controllers/CampainController.php
@@ -2,11 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Http\Request;
 use Auth;
 use App\Campain;
+use App\Company;
 
-class CampainController extends BaseController
+class CampainController extends Controller
 {
     public function index()
     {
@@ -20,8 +21,37 @@ class CampainController extends BaseController
     public function create()
     {
         $userName = Auth::user()->name;
+        $companies = Company::all();
         
-        return view('company.create', ['userName' => $userName]);
+        return view('campain.create', ['userName' => $userName, 'companies' => $companies]);
+    }
+
+    public function store(Request $request)
+    {
+        $userName = Auth::user()->name;
+
+        $this->validate($request, [
+                'start_at'      => 'required',
+                'end_at'        => 'required',
+                'description'   => 'required',
+                'target'        => 'required',
+                'advert'        => 'required'
+            ]);
+
+        $input = $request->all();
+
+        $campain = new Campain;
+        $campain->start_at = $input['start_at'];
+        $campain->end_at = $input['end_at'];
+        $campain->description = $input['description'];
+        $campain->cost = $input['cost'];
+        $campain->gain = $input['gain'];
+        $campain->target = $input['target'];
+        $campain->advert = $input['advert'];
+        $campain->company_id = $input['company_id'];
+        $campain->save();
+
+        return redirect('campain/');
     }
 
     public function optAdd()
--- resources/views/campain/create.blade.php
+++ resources/views/campain/create.blade.php
@@ -7,27 +7,55 @@
 	<div class="col-sm-12">
 		<div class="the-box">
 		<h4 class="small-title">AGREGAR</h4>
-			<form role="form">
+			@if (count($errors) > 0)
+			    <div class="alert alert-danger">
+			        <ul>
+			            @foreach ($errors->all() as $error)
+			                <li>{{ $error }}</li>
+			            @endforeach
+			        </ul>
+			    </div>
+			@endif
+			<form role="form" method="POST" action="/campain/">
 			  <div class="form-group">
-				<label>Nombre</label>
-				<input type="text" class="form-control">
+				<label>Inicio</label>
+				<input type="text" name="start_at" class="form-control datepicker" data-date-format="yyyy-mm-dd" placeholder="yyyy-mm-dd">
+			  </div>
+			  <div class="form-group">
+				<label>Fin</label>
+				<input type="text" name="end_at" class="form-control datepicker" data-date-format="yyyy-mm-dd" placeholder="yyyy-mm-dd">
 			  </div>
 			  <div class="form-group">
 				<label>Descripción</label>
-				<input type="text" class="form-control">
+				<input type="text" name="description" class="form-control">
 			  </div>
 			  <div class="form-group">
-				<label>Teléfono</label>
-				<input type="text" class="form-control phone_us_masking" placeholder="(000) 000-0000">
+				<label>Costo</label>
+				<input type="text" name="cost" class="form-control">
 			  </div>
 			  <div class="form-group">
-				<label>Email</label>
-				<input type="email" class="form-control">
+				<label>Beneficio</label>
+				<input type="text" name="gain" class="form-control">
 			  </div>
 			  <div class="form-group">
-				<label>Usuario Coordinador</label>
-				<input type="text" class="form-control">
+				<label>Objetivo</label>
+				<input type="text" name="target" class="form-control">
 			  </div>
+			  <div class="form-group">
+				<label>Texto</label>
+				<textarea name="advert" class="form-control"></textarea>
+			  </div>
+
+			  <div class="form-group">
+				<label>Empresa</label>
+				<select class="form-control" name="company_id">
+					@foreach($companies as $company)
+						<option value="{{$company->id}}">{{$company->name}}</option>
+					@endforeach
+				</select>
+			  </div>
+
+			  <input type="hidden" name="_token" value="{{ csrf_token() }}">
 			  
 			  <button type="submit" class="btn btn-danger"><i class="fa fa-sign-in"></i> Agregar</button>
 			</form>
